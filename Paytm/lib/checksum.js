e25cf8608f3e3e30c59c92b5eec765b0a3ceb37a
Debasis Tripathy <debasis.tripathy@imss.co.in>
2024-03-26 10:29:15 +0530

    added paytm be

--- Paytm/lib/checksum.js
+++ Paytm/lib/checksum.js
@@ -51,9 +51,115 @@ const verifyCheckSum = (params, key, checkSumHash )=>{
         checkSumHash = checkSumHash.replace("\r", "");
         let temp = decodeURIComponent(checkSumHash);
         let checksum = crypto.publicDecrypt(temp, key)
+        let salt = checksum.substr(0, checksum.length - 4);
+        let sha256 = checksum.substr(0, checksum - 4);
+        let hash = crypto.createHash("sha256").update(params+"|"+salt).digest("hex")
+
+        if(hash === sha256){
+            return true;
+        }
+        else{
+            util.log("Checksum is not matching");
+            return false
+        }
+    }
+
+    const verifyCheckSumSignature = (params, key, checksum)=>{
         
     }
 }
 
 
+// const crypto = require('crypto');
+
+// class PaytmChecksum {
+//     static async encrypt(input, key) {
+//         const cipher = crypto.createCipheriv('aes-128-cbc', key, PaytmChecksum.iv);
+//         let encrypted = cipher.update(input, 'binary', 'base64');
+//         encrypted += cipher.final('base64');
+//         return encrypted;
+//     }
+
+//     static async decrypt(encrypted, key) {
+//         const decipher = crypto.createDecipheriv('aes-128-cbc', key, PaytmChecksum.iv);
+//         let decrypted = decipher.update(encrypted, 'base64', 'binary');
+//         try {
+//             decrypted += decipher.final('binary');
+//         } catch (e) {
+//             console.log(e);
+//         }
+//         return decrypted;
+//     }
+
+//     static async generateSignature(params, key) {
+//         if (typeof params !== "object" && typeof params !== "string") {
+//             const error = "string or object expected, " + (typeof params) + " given.";
+//             return Promise.reject(error);
+//         }
+//         if (typeof params !== "string") {
+//             params = PaytmChecksum.getStringByParams(params);
+//         }
+//         return PaytmChecksum.generateSignatureByString(params, key);
+//     }
+
+//     static async verifySignature(params, key, checksum) {
+//         if (typeof params !== "object" && typeof params !== "string") {
+//             const error = "string or object expected, " + (typeof params) + " given.";
+//             return Promise.reject(error);
+//         }
+//         if (params.hasOwnProperty("CHECKSUMHASH")) {
+//             delete params.CHECKSUMHASH;
+//         }
+//         if (typeof params !== "string") {
+//             params = PaytmChecksum.getStringByParams(params);
+//         }
+//         return PaytmChecksum.verifySignatureByString(params, key, checksum);
+//     }
+
+//     static async generateSignatureByString(params, key) {
+//         const salt = await PaytmChecksum.generateRandomString(4);
+//         return PaytmChecksum.calculateChecksum(params, key, salt);
+//     }
+
+//     static async verifySignatureByString(params, key, checksum) {
+//         const paytm_hash = await PaytmChecksum.decrypt(checksum, key);
+//         const salt = paytm_hash.substr(paytm_hash.length - 4);
+//         return (paytm_hash === PaytmChecksum.calculateHash(params, salt));
+//     }
+
+//     static async generateRandomString(length) {
+//         return new Promise((resolve, reject) => {
+//             crypto.randomBytes((length * 3.0) / 4.0, (err, buf) => {
+//                 if (!err) {
+//                     const salt = buf.toString("base64");
+//                     resolve(salt);
+//                 } else {
+//                     console.log("error occurred in generateRandomString: " + err);
+//                     reject(err);
+//                 }
+//             });
+//         });
+//     }
+
+//     static getStringByParams(params) {
+//         const data = {};
+//         Object.keys(params).sort().forEach(function (key) {
+//             data[key] = (params[key] !== null && params[key].toLowerCase() !== "null") ? params[key] : "";
+//         });
+//         return Object.values(data).join('|');
+//     }
+
+//     static calculateHash(params, salt) {
+//         const finalString = params + "|" + salt;
+//         return crypto.createHash('sha256').update(finalString).digest('hex') + salt;
+//     }
+
+//     static calculateChecksum(params, key, salt) {
+//         const hashString = PaytmChecksum.calculateHash(params, salt);
+//         return PaytmChecksum.encrypt(hashString, key);
+//     }
+// }
+// PaytmChecksum.iv = '@@@@&&&&####$$$$';
+
+// module.exports = PaytmChecksum;
 
